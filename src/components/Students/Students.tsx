9f1b7df84fbbb9984193886b65fdd168ba7bef89
TSHmmTS <kudryavtsevva@mer.ci.nsu.ru>
2025-09-29 16:07:02 +0700

    Добавлена кнопка удаления студента.

--- src/components/Students/Students.tsx
+++ src/components/Students/Students.tsx
@@ -3,18 +3,23 @@
 import useStudents from '@/hooks/useStudents';
 import type StudentInterface from '@/types/StudentInterface';
 import styles from './Students.module.scss';
+import Student from './Student/Student';
 
 const Students = (): React.ReactElement => {
-  const { students } = useStudents();
+  const { students, deleteStudentMutate } = useStudents();
+
+  const onDeleteHandler = (studentId: number) => {
+    deleteStudentMutate(studentId);
+  }
 
   return (
     <div className={styles.Students}>
       {students.map((student: StudentInterface) => (
-        <h2 key={student.id}>
-          {student.lastName + '\n'}
-          {student.firstName + '\n'}
-          {student.middleName}
-        </h2>
+        <Student
+          key={student.id}
+          student={student}
+          onDelete={onDeleteHandler}
+        />
       ))}
     </div>
   );
